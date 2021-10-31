85ae41d61905c6431c4828a3574d90a9ff673942
hectorvcode <hectorfabiovasquez@gmail.com>
2021-10-31 09:41:04 -0500

    Created controller methods and services

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,14 +1,62 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, NotFoundException, Param, Post, Put, Query, Res } from '@nestjs/common';
 import { CreateProductDto } from './dto/product.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
 
+    constructor(private productService: ProductService){}
+
     @Post('/create')
-    createPost(@Res() res, @Body() createProductDto: CreateProductDto){
-        console.log(createProductDto);
+    async createPost(@Res() res, @Body() createProductDto: CreateProductDto){
+        const product = await this.productService.createProduct(createProductDto);
         return res.status(HttpStatus.OK).json({
-            message: 'received'
+            message: 'Succesfully Created',
+            product: product
         })
     }
+
+    @Get()
+    async getProducts(@Res() res){
+        const products = await this.productService.getProducts();
+        return res.status(HttpStatus.OK).json({
+            products
+        })
+    }
+
+    @Get('/:productID')
+    async getProduct(@Res() res, @Param('productID') productID){
+        const product = await this.productService.getProduct(productID);
+        if(!product){
+            throw new NotFoundException('Product does not exist')
+        }
+        return res.status(HttpStatus.OK).json({
+            product
+        })
+    }
+
+    @Delete('delete')
+    async deleteProduct(@Res() res, @Query('productID') productID){
+        const productDeleted = await this.productService.deleteProduct(productID);
+        if(!productDeleted) throw new NotFoundException('Product does not exist');
+        return res.status(HttpStatus.OK).json({
+            message: 'Product Deleted Succesfully',
+            productDeleted
+        })
+    }
+
+    @Put('/update')
+    async updateProduct(@Res() res, @Body() createProductDto:CreateProductDto, @Query('productID') productID){
+        const updatedProduct = await this.productService.updateProduct(productID, createProductDto);
+        if(!updatedProduct) throw new NotFoundException('Product does not exist');
+        return res.status(HttpStatus.OK).json({
+            message: 'Product Updated Successfully',
+            updatedProduct
+        })
+    }
+
 }
+
+/** 58:30
+ * https://www.youtube.com/watch?v=jEKsD5f3Bqc
+ */
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateProductDto } from './dto/product.dto';
+import { Product } from './interfaces/product.interface';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel('Product') private readonly productModel: Model<Product>,
+  ) {}
+
+  async getProducts(): Promise<Product[]> {
+    const products = await this.productModel.find();
+    return products;
+  }
+
+  async getProduct(productID: string): Promise<Product> {
+    const product = await this.productModel.findById(productID);
+    return product;
+  }
+
+  async createProduct(createProductDto: CreateProductDto): Promise<Product> {
+    const product = new this.productModel(createProductDto);
+    return await product.save();
+  }
+
+  async deleteProduct(productID: string): Promise<Product> {
+    const deletedProduct = await this.productModel.findByIdAndDelete(productID);
+    return deletedProduct;
+  }
+
+  async updateProduct(
+    productID: string,
+    createProductDto: CreateProductDto,
+  ): Promise<Product> {
+    const updatedProduct = await this.productModel.findByIdAndUpdate(
+      productID,
+      createProductDto,
+      { new: true },
+    );
+    return updatedProduct;
+  }
+}
